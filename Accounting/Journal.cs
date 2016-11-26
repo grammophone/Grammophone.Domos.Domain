b34ecf4d5ff59adb18a031f048430e624ad86c29
grammophone <emmanuel.liossis@gmail.com>
2016-11-27 00:36:28 +0200

    Improve Journal comments about type paramters.

--- Accounting/Journal.cs
+++ Accounting/Journal.cs
@@ -11,14 +11,16 @@ namespace Grammophone.Domos.Domain.Accounting
 	/// Base for accounting journals, having group ownership.
 	/// </summary>
 	/// <typeparam name="U">The type of the user, derived from <see cref="User"/>.</typeparam>
-	/// <typeparam name="ST">The type of state transition, derived from <see cref="Workflow.StateTransition{U}"/>.</typeparam>
+	/// <typeparam name="BST">
+	/// The base type of system's state transitions, derived from <see cref="Workflow.StateTransition{U}"/>.
+	/// </typeparam>
 	/// <typeparam name="A">The type of accounts, derived from <see cref="Account{U}"/>.</typeparam>
 	/// <typeparam name="P">The type of the postings, derived from <see cref="Posting{U, A}"/>.</typeparam>
 	/// <typeparam name="R">The type of remittances, derived from <see cref="Remittance{U, A}"/>.</typeparam>
 	[Serializable]
-	public abstract class Journal<U, ST, A, P, R> : UserGroupTrackingEntityWithID<U, long>
+	public abstract class Journal<U, BST, A, P, R> : UserGroupTrackingEntityWithID<U, long>
 		where U : User
-		where ST : Workflow.StateTransition<U>
+		where BST : Workflow.StateTransition<U>
 		where A : Account<U>
 		where P : Posting<U, A>
 		where R : Remittance<U, A>
@@ -50,7 +52,7 @@ namespace Grammophone.Domos.Domain.Accounting
 		/// <summary>
 		/// Optional workflow state transition associated with this journal.
 		/// </summary>
-		public virtual ST StateTransition { get; set; }
+		public virtual BST StateTransition { get; set; }
 
 		/// <summary>
 		/// The postings in this journal.
