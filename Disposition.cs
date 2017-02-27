244eb31e62460e66c070e421b4d178b1376303d6
grammophone <emmanuel.liossis@gmail.com>
2017-02-28 00:40:03 +0200

    Implement IDisposition by base Disposition class.

--- Disposition.cs
+++ Disposition.cs
@@ -82,7 +82,7 @@ namespace Grammophone.Domos.Domain
 	/// <typeparam name="U">The type of the user.</typeparam>
 	/// <typeparam name="S">The type of segregation, derived from <see cref="Segregation{U}"/>.</typeparam>
 	[Serializable]
-	public abstract class Disposition<U, S> : Disposition
+	public abstract class Disposition<U, S> : Disposition, IDisposition<U, S>
 		where U : User
 		where S : Segregation<U>
 	{
@@ -92,6 +92,14 @@ namespace Grammophone.Domos.Domain
 		/// The segregation.
 		/// </summary>
 		public virtual S Segregation { get; set; }
+
+		/// <summary>
+		/// Maps to setting the <see cref="Segregation"/>.
+		/// </summary>
+		void IDisposition<U, S>.SetSegregation(S segregation)
+		{
+			this.Segregation = segregation;
+		}
 
 		#endregion
 	}
