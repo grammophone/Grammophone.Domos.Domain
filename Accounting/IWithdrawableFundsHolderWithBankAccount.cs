a91a26c2c29b7f2c2b3086ade10249ea17eb4d8c
grammophone <emmanuel.liossis@gmail.com>
2017-03-02 17:52:08 +0200

    Add withdrawable funds holder interfaces.

---
+++ Accounting/IWithdrawableFundsHolderWithBankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Domos.Domain.Accounting
+{
+	/// <summary>
+	/// Contract of an account system with banking information
+	/// from which amounts can be withdrawn
+	/// </summary>
+	/// <typeparam name="U">The type of users, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="A">The base type of accounts, derived from <see cref="Account{U}"/>.</typeparam>
+	public interface IWithdrawableFundsHolderWithBankAccount<U, A> 
+		: IWithdrawableFundsHolder<U, A>
+		where U : User
+		where A : Account<U>
+	{
+		/// <summary>
+		/// The banking information.
+		/// </summary>
+		IBankAccountHolder BankingDetail { get; }
+	}
+}
