1e5075fda526ecb25d1423143683f887de81f9e8
grammophone <emmanuel.liossis@gmail.com>
2017-05-31 21:36:56 +0300

    Add display names to TrackingEntity properties.

--- TrackingEntity.cs
+++ TrackingEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -29,6 +30,9 @@ namespace Grammophone.Domos.Domain
 		/// Once set, cannot be changed.
 		/// </summary>
 		[IgnoreDataMember]
+		[Display(
+			ResourceType = typeof(TrackingEntityResources),
+			Name = nameof(TrackingEntityResources.CreationDate_Name))]
 		public virtual DateTime CreationDate
 		{
 			get
@@ -46,6 +50,9 @@ namespace Grammophone.Domos.Domain
 		/// Set by the system.
 		/// </summary>
 		[IgnoreDataMember]
+		[Display(
+			ResourceType = typeof(TrackingEntityResources),
+			Name = nameof(TrackingEntityResources.CreationDate_Name))]
 		public virtual DateTime LastModificationDate
 		{
 			get
