9b367a556c454db496ca1756abd87d4241e98e67
grammophone <emmanuel.liossis@gmail.com>
2017-07-27 01:41:19 +0300

    Fix StateGroupResources visibility.

--- Workflow/StateGroupResources.Designer.cs
+++ Workflow/StateGroupResources.Designer.cs
@@ -22,7 +22,7 @@ namespace Grammophone.Domos.Domain.Workflow {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class StateGroupResources {
+    public class StateGroupResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Grammophone.Domos.Domain.Workflow {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Grammophone.Domos.Domain.Workflow.StateGroupResources", typeof(StateGroupResources).Assembly);
@@ -51,7 +51,7 @@ namespace Grammophone.Domos.Domain.Workflow {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Grammophone.Domos.Domain.Workflow {
         /// <summary>
         ///   Looks up a localized string similar to Code Name.
         /// </summary>
-        internal static string CodeName_Name {
+        public static string CodeName_Name {
             get {
                 return ResourceManager.GetString("CodeName_Name", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace Grammophone.Domos.Domain.Workflow {
         /// <summary>
         ///   Looks up a localized string similar to Description.
         /// </summary>
-        internal static string Description_Name {
+        public static string Description_Name {
             get {
                 return ResourceManager.GetString("Description_Name", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace Grammophone.Domos.Domain.Workflow {
         /// <summary>
         ///   Looks up a localized string similar to State Group Name.
         /// </summary>
-        internal static string Name_Name {
+        public static string Name_Name {
             get {
                 return ResourceManager.GetString("Name_Name", resourceCulture);
             }
