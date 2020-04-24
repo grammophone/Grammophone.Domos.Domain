cf5b9cb49b90650a8cf57a4a95a6852cf8344ee9
grammophone <emmanuel.liossis@gmail.com>
2020-04-24 17:08:44 -0400

    Fix resources of InvoiceType enumeration to be public.

--- Accounting/InvoiceTypeResources.Designer.cs
+++ Accounting/InvoiceTypeResources.Designer.cs
@@ -19,10 +19,10 @@ namespace Grammophone.Domos.Domain.Accounting {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class InvoiceTypeResources {
+    public class InvoiceTypeResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Grammophone.Domos.Domain.Accounting {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Grammophone.Domos.Domain.Accounting.InvoiceTypeResources", typeof(InvoiceTypeResources).Assembly);
@@ -51,7 +51,7 @@ namespace Grammophone.Domos.Domain.Accounting {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Grammophone.Domos.Domain.Accounting {
         /// <summary>
         ///   Looks up a localized string similar to Normal Charge.
         /// </summary>
-        internal static string NormalCharge_Name {
+        public static string NormalCharge_Name {
             get {
                 return ResourceManager.GetString("NormalCharge_Name", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace Grammophone.Domos.Domain.Accounting {
         /// <summary>
         ///   Looks up a localized string similar to Reverse Charge.
         /// </summary>
-        internal static string ReverseCharge_Name {
+        public static string ReverseCharge_Name {
             get {
                 return ResourceManager.GetString("ReverseCharge_Name", resourceCulture);
             }
