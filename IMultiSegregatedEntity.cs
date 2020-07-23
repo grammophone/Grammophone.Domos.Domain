ae1c73eb44e9a15c876e980cfda95ad74614a508
grammophone <emmanuel.liossis@gmail.com>
2020-07-22 21:54:30 -0400

    Add IMultiSegregatedEntity interface.

---
+++ IMultiSegregatedEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grammophone.Domos.Domain
+{
+	/// <summary>
+	/// Contract for entities belonging to multiple segregations.
+	/// </summary>
+	public interface IMultiSegregatedEntity
+	{
+		/// <summary>
+		/// The collection of IDs of the abstract segregations where this entity belongs to.
+		/// For example, in a platform-as-a-service scenario, these could be the IDs of the companies where the entity belongs.
+		/// </summary>
+		IEnumerable<long> SegregationIDs { get; }
+	}
+}
